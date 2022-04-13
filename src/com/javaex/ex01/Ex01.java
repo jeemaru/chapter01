f666bd92e53a787573df281cfc8fb86fadc92f4a
dkdzm <dkdzm@175.113.137.4>
2022-04-13 22:58:18 +0900

    1

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,56 @@
+/*
+작성자: 김지수
+작성일: 20220-04-13
+설 명: 나이기록 프로그램
+*/
+
+package com.javaex.ex01;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		//int myAge = 25; 변수선언 + 초기화
+		
+		/*int myAge; 변수선언
+		myAge = 25; 초기화
+		
+		// 메모리의 값을 출력하는 메소드
+		System.out.println(myAge);
+		
+		myAge = 26;
+		
+		System.out.println(myAge);*/
+		
+		/*byte no;
+		no = -128;
+		System.out.println(no);
+		
+		int 범위까지는 l을 붙이지 않아도 된다
+		long no2;
+		no2 = 12312312310l;
+		System.out.println(no2);*/
+		
+		/*long no2;
+		no2 = 922337203;
+		System.out.println(no2);*/
+		
+		//변수가 여러개일떄 변수선언, 초기화
+		int var01; 
+		int var02; 
+		int var03;
+		//int var01, var02, var03; <-- 이미있는 선언값은 적용안됨
+		
+		var01 = 10;
+		var02 = 20;
+		var03 = 30;
+		
+		//변수가 여러개일때 변수선언 + 초기화
+		int var04 = 10;
+		int var05 = 20;
+		int var06 = 30;
+		//int var04=10, var05=20, var=30;
+		System.out.println(var04);
+	}
+
+}
---
+++ src/com/javaex/ex01/Ex04.java
@@ -0,0 +1,53 @@
+package com.javaex.ex01;
+
+public class Ex04 {
+	
+	public static void main(String[]args) {
+		
+		char ch01 = 'A'; //글자를 쓸때는 항상 작음 따옴표
+		char ch02 = '안'; //텍스트는 코드표를 찾고 해당 코드입력후 텍스트 출력
+		
+		System.out.println(ch01);
+		System.out.println(ch02);
+		
+		char ch03 = 65;
+		
+		System.out.println(ch03);
+		
+		char ch04 = '3';
+				
+		System.out.println(ch04);
+		
+		String str = "뭐든지 해드립니다. 연락좀 02-555-6666"; //<--복수의 텍스트 표현할떄 쓰는 변수선언, 초기화방법 따옴표는 두개짜리!!!
+		
+		System.out.println(str);
+		
+		String number = "110812"; // 학번
+		String name = "김지수"; // 이름
+		boolean isEnrolled = true; // 등록 여부
+		double grade = 3.5; // 평점
+		String address = "난곡동 골다방"; // 주소
+		String major = "간호학과"; // 전공
+		double unit = 2.7; // 이수 학점
+		boolean haveMinor = false; // 부전공 여부
+		long juminNo = 9312311235468l; // 주민번호
+		String cellphone = "010999988888"; // 핸드폰 번호
+		int age = 30; // 나이
+		String email = "4iiii@hanmail.net"; // 이메일주소
+		
+		System.out.println(number);
+		System.out.println(name);
+		System.out.println(isEnrolled);
+		System.out.println(grade);
+		System.out.println(address);
+		System.out.println(major);
+		System.out.println(unit);
+		System.out.println(haveMinor);
+		System.out.println(juminNo);
+		System.out.println(cellphone);
+		System.out.println(age);
+		System.out.println(email);
+		
+	}
+
+}
